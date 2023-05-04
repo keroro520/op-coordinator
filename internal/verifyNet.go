7e0ab51fd4a93fcc045f42aa775184d5f068d645
eric <eric.l@nodereal.io>
2023-05-04 14:54:52 +0800

    dev: add net check

---
+++ internal/verifyNet.go
@@ -0,0 +1,14 @@
+package internal
+
+/*
+*
+param
+ 1. func()(bridge list, suqencers)
+*/
+func verifyNetValiable() {
+
+	/**
+		 1. bridge  >=1/2  is valiable && sequencer >=1/2  is valiable sequencer
+	     2. alert
+	*/
+}
