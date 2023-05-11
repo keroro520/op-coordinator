78ffdbdf0c46347ea15517169043ac249b6e69a6
keroro <keroroxx520@gmail.com>
2023-05-11 10:57:51 +0800

    feat: precheck setMaster

--- internal/admin.go
+++ internal/admin.go
@@ -1,5 +1,7 @@
 package internal
 
+import "errors"
+
 // AdminCommand is the interface for admin commands. Admin commands come from HTTP API and are executed by
 // Coordinator synchronously.
 type AdminCommand interface {
@@ -21,8 +23,17 @@ func NewSetMasterCommand(nodeName string) SetMasterCommand {
 }
 
 func (cmd SetMasterCommand) Execute(coordinator *Coordinator) {
+	if !coordinator.isCandidate(cmd.nodeName) {
+		cmd.RespCh() <- errors.New("node is not a candidate")
+		return
+	}
+	if coordinator.master == cmd.nodeName {
+		cmd.RespCh() <- errors.New("node is already the master")
+		return
+	}
+
 	coordinator.revokeCurrentMaster()
-	cmd.respCh <- coordinator.setMaster(cmd.nodeName)
+	cmd.RespCh() <- coordinator.setMaster(cmd.nodeName)
 }
 
 func (cmd SetMasterCommand) RespCh() chan error {
