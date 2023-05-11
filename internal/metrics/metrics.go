8c61fb8139d217555fc54beaeb1a744a7668f217
keroro <keroroxx520@gmail.com>
2023-05-11 16:10:09 +0800

    feat: add metrics

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,89 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
+
+var (
+	Namespace = ""
+	Subsystem = "coordinator"
+)
+
+var (
+	MetricElections = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "elections_count_total",
+		Help:      "Tracks total elections",
+	})
+	MetricElectionFailures = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "election_failures_count_total",
+		Help:      "Tracks total election failures",
+	})
+	MetricElectionDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "election_duration_ms",
+		Buckets:   prometheus.DefBuckets,
+		Help:      "Tracks election duration in milliseconds",
+	})
+	MetricWaitingConvergenceDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "waiting_convergence_duration_ms",
+		Buckets:   prometheus.DefBuckets,
+		Help:      "Tracks waiting convergence duration in milliseconds",
+	})
+	MetricHealthCheckFailures = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "hc_failures_count",
+		Help:      "Tracks health check failures",
+	}, []string{"node"})
+	MetricHealthCheckOpNodeDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "hc_op_node_duration_ms",
+		Buckets:   prometheus.DefBuckets,
+	}, []string{"node"})
+	MetricHealthCheckOpGethDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "hc_op_geth_duration_ms",
+		Buckets:   prometheus.DefBuckets,
+	}, []string{"node"})
+
+	MetricHTTPRequestDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "http_request_duration_ms",
+		Help:      "Tracks HTTP request durations, in ms",
+		Buckets:   prometheus.DefBuckets,
+	}, []string{"method", "status_code"})
+	MetricHTTPRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "http_requests_count_total",
+		Help:      "Tracks total HTTP requests",
+	}, []string{"method"})
+	MetricHTTPResponses = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Subsystem: Subsystem,
+		Name:      "http_responses_count_total",
+		Help:      "Tracks total HTTP responses",
+	}, []string{"method", "status_code"})
+)
+
+func StartMetrics(addr string) {
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			panic(err)
+		}
+	}()
+}
--- internal/server.go
+++ internal/server.go
@@ -2,8 +2,9 @@ package internal
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"github.com/ethereum-optimism/optimism/op-service/httputil"
+	"github.com/node-real/op-coordinator/internal/metrics"
 	"net"
 	"net/http"
 	"strconv"
@@ -26,7 +27,7 @@ func NewHttpServer(handler http.Handler) *http.Server {
 	}
 }
 
-type rpcServer struct {
+type RpcServer struct {
 	endpoint   string
 	apis       []rpc.API
 	httpServer *http.Server
@@ -34,10 +35,10 @@ type rpcServer struct {
 	listenAddr net.Addr
 }
 
-func NewRPCServer(rpcCfg RpcConfig, appVersion string, c *Coordinator) (*rpcServer, error) {
+func NewRPCServer(rpcCfg RpcConfig, appVersion string, c *Coordinator) *RpcServer {
 	api := NewNodeAPI(appVersion, c)
 	endpoint := net.JoinHostPort(rpcCfg.Host, strconv.Itoa(rpcCfg.Port))
-	r := &rpcServer{
+	r := &RpcServer{
 		endpoint: endpoint,
 		apis: []rpc.API{{
 			Namespace:     "coordinator",
@@ -47,32 +48,51 @@ func NewRPCServer(rpcCfg RpcConfig, appVersion string, c *Coordinator) (*rpcServ
 		}},
 		appVersion: appVersion,
 	}
-	return r, nil
+	return r
 }
 
-func (s *rpcServer) Start() error {
+func NewHTTPRecordingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		metrics.MetricHTTPRequests.WithLabelValues(r.Method).Inc()
+		ww := httputil.NewWrappedResponseWriter(w)
+		start := time.Now()
+		next.ServeHTTP(ww, r)
+		duration := time.Since(start)
+		metrics.MetricHTTPResponses.WithLabelValues(r.Method, strconv.Itoa(ww.StatusCode)).Inc()
+		metrics.MetricHTTPRequestDuration.WithLabelValues(r.Method, strconv.Itoa(ww.StatusCode)).Observe(float64(duration.Milliseconds()))
+	})
+}
+
+func (s *RpcServer) Start() error {
 	srv := rpc.NewServer()
 	if err := node.RegisterApis(s.apis, nil, srv); err != nil {
 		return err
 	}
 	nodeHandler := node.NewHTTPHandlerStack(srv, []string{"*"}, []string{"*"}, nil)
+	nodeHandler = NewHTTPRecordingMiddleware(nodeHandler)
+
 	mux := http.NewServeMux()
 	mux.Handle("/", nodeHandler)
-	mux.HandleFunc("/healthz", healthzHandler(s.appVersion))
-	listener, err := net.Listen("tcp", s.endpoint)
-	if err != nil {
-		return err
+	mux.HandleFunc("/healthz", HealthzHandler(s.appVersion))
+
+	s.httpServer = &http.Server{
+		Handler:           mux,
+		Addr:              s.endpoint,
+		ReadTimeout:       timeouts.ReadTimeout,
+		ReadHeaderTimeout: timeouts.ReadHeaderTimeout,
+		WriteTimeout:      timeouts.WriteTimeout,
+		IdleTimeout:       timeouts.IdleTimeout,
 	}
-	s.listenAddr = listener.Addr()
-	s.httpServer = NewHttpServer(mux)
 	go func() {
-		if err := s.httpServer.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			//s.Error("http server failed", "err", err)
+		if err := s.httpServer.ListenAndServe(); err != nil {
+			s.httpServer.ErrorLog.Printf("HTTP server exit, error: %v", err)
 		}
 	}()
+
 	return nil
 }
-func healthzHandler(appVersion string) http.HandlerFunc {
+
+func HealthzHandler(appVersion string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte(appVersion))
 	}
@@ -95,6 +115,7 @@ func (n *nodeAPI) RequestBuildingBlock(nodeName string) error {
 	if n.coordinator.master == "" {
 		return fmt.Errorf("empty master")
 	}
+
 	if n.coordinator.master != nodeName {
 		go func() {
 			zap.S().Warnw("Invalid master is requesting!", "master", n.coordinator.master, "node", nodeName)
@@ -126,6 +147,7 @@ func (n *nodeAPI) SetMaster(nodeName string) error {
 	return executeAdminCommand(ctx, n.coordinator, NewSetMasterCommand(nodeName))
 }
 
+// executeAdminCommand executes an admin command and returns the result.
 func executeAdminCommand(ctx context.Context, coordinator *Coordinator, cmd AdminCommand) error {
 	coordinator.AdminCh() <- cmd
 
